83041ab61657ae8c0549710d70c3cf3b17eb5fd0
Amirhossein Bayani <amirhossein.bayani@gmail.com>
2024-10-23 10:59:40 +0000

    start the pong game

---
+++ main.py
@@ -0,0 +1,28 @@
+from turtle import Turtle, Screen
+from paddle import Paddles
+
+
+screen = Screen()
+screen.bgcolor("black")
+screen.setup(800,600)
+screen.title("Pong Game")
+screen.tracer(0)
+
+r_paddle = Paddles(350,0)
+l_paddle = Paddles(-350,0)
+
+
+
+screen.listen()
+screen.onkey(r_paddle.go_up, "Up")
+screen.onkey(r_paddle.go_down, "Down")
+screen.onkey(l_paddle.go_up, "w")
+screen.onkey(l_paddle.go_down, "s")
+
+game_is_on = True
+
+while game_is_on:
+    screen.update()
+
+
+screen.exitonclick()
